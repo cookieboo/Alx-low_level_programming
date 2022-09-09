953b9e246d63844cba2f2d92375d141605e8ce06
cookieboo <onyejiakakhandy@gmail.com>
2022-09-08 19:13:35 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main- Serves as the entru point for the program
- *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-puts("\"Programming is like  building a multilingual puzzle");
-return(o);
+	put ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
