dbed0f546daf900b9ff7c6d67f0bbd1215d1b375
cookieboo <onyejiakakhandy@gmail.com>
2022-09-09 19:03:23 -0700

    assign a random number to the variable clear

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
 * main - print if the number is positive, zero, or negative
 *
-Description: using rhe main function
+Description: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
@@ -14,7 +14,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n - rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
   /* your code goes there */
 if (n > 0)
 {
